bd4a3ef12a814dbb8aa627a1b941e95c1cdf0e73
Yuki.kaco <a0936660023@gmail.com>
2024-05-01 01:18:16 +0800

    Add New Feature Coin,Health,projectileAttribute,Developer Window

--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,10 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.IL2CPP;
 using HarmonyLib;
 using System;
 using UnhollowerRuntimeLib;
+using UnityEngine;
 
 namespace ArrowPlugin;
 
@@ -10,8 +12,19 @@ namespace ArrowPlugin;
 public class Plugin : BasePlugin
 {
     public const string PLUGIN_GUID = "ArrowPlugin";
-    public const string PLUGIN_NAME = "ArrowPlugin";
+    public const string PLUGIN_NAME = "Yuki ArrowPlugin";
     public const string PLUGIN_VERSION = "1.0.0";
+
+    public static int defaultValue = 1337;
+
+    public static ConfigEntry<bool> debugPanel;
+    public static ConfigEntry<int> Coin;
+    public static ConfigEntry<float> Health;
+    public static ConfigEntry<float> animationSpeed;
+    public static ConfigEntry<int> projectileCount;
+    public static ConfigEntry<float> projectileDamage;
+    public static ConfigEntry<float> projectileDistance;
+    public static ConfigEntry<float> projectileSpeed;
 
     public static Plugin Instance;
 
@@ -33,6 +46,33 @@ public class Plugin : BasePlugin
             Log.LogMessage("Failed to Registers Il2cpp Type");
         }
 
+        debugPanel = Config.Bind<bool>("", "Developer Window", false, "");
+        Coin = Config.Bind<int>("", "Coin", defaultValue, "");
+        Health = Config.Bind<float>("", "Health", 400, "");
+        animationSpeed = Config.Bind<float>("", "shootFrequency", 10, "");
+        projectileCount = Config.Bind<int>("", "projectileCount", 1, "");
+        projectileDamage = Config.Bind<float>("", "projectileDamage", 14, "");
+        projectileDistance = Config.Bind<float>("", "projectileDistance", 10, "");
+        projectileSpeed = Config.Bind<float>("", "projectileSpeed", 6, "");
+
+        debugPanel.Value = false;
+        Coin.Value = defaultValue;
+        Health.Value = defaultValue;
+        animationSpeed.Value = 10;
+        projectileCount.Value = 1;
+        projectileDamage.Value = 14;
+        projectileDistance.Value = 10;
+        projectileSpeed.Value = 6;
+
+        debugPanel.SettingChanged += DebugPanel_SettingChanged;
+        Coin.SettingChanged += Coin_SettingChanged;
+        Health.SettingChanged += Health_SettingChanged;
+        animationSpeed.SettingChanged += AnimationSpeed_SettingChanged;
+        projectileCount.SettingChanged += ProjectileCount_SettingChanged;
+        projectileDamage.SettingChanged += ProjectileDamage_SettingChanged;
+        projectileDistance.SettingChanged += ProjectileDistance_SettingChanged;
+        projectileSpeed.SettingChanged += ProjectileSpeed_SettingChanged;
+        
         try
         {
             var harmony = new Harmony(PLUGIN_GUID);
@@ -44,6 +84,54 @@ public class Plugin : BasePlugin
         {
             Log.LogMessage("Failed to Apply Hook");
         }
+    }
+
+    private void DebugPanel_SettingChanged(object sender, EventArgs e)
+    {
+        if (GameObject.Find("Canvas").transform.Find("debugPanel").gameObject)
+        {
+            GameObject.Find("Canvas").transform.Find("debugPanel").gameObject.SetActive(debugPanel.Value);
+        }
+        
+    }
+
+    private void Coin_SettingChanged(object sender, EventArgs e)
+    {
+        //Log.LogMessage($"Money:{GameDataManager.money} {CoinValue.Value}");
+        GameDataManager.money = Coin.Value;
+    }
+
+    private void Health_SettingChanged(object sender, EventArgs e)
+    {
+        GameManager.Instance.playerInteraction.health = Health.Value;
+    }
+
+    private void AnimationSpeed_SettingChanged(object sender, EventArgs e)
+    {
+        GameManager.Instance.playerInteraction.animationSpeed = animationSpeed.Value;
+        GameManager.Instance.playerInteraction.AddAnimatorSpeed(0);
+    }
+
+    private void ProjectileCount_SettingChanged(object sender, EventArgs e)
+    {
+        GameManager.Instance.playerInteraction.projectileCount = projectileCount.Value;
+        GameManager.Instance.playerInteraction.AddProjectileCount(0);
+    }
+
+    private void ProjectileDamage_SettingChanged(object sender, EventArgs e)
+    {
+        GameManager.Instance.playerInteraction.projectileDamage = projectileDamage.Value;
+    }
+
+
+    private void ProjectileDistance_SettingChanged(object sender, EventArgs e)
+    {
+        GameManager.Instance.playerInteraction.projectileDistance = projectileDistance.Value;
+    }
+
+    private void ProjectileSpeed_SettingChanged(object sender, EventArgs e)
+    {
+        GameManager.Instance.playerInteraction.projectileSpeed = projectileSpeed.Value;
     }
 
     class BootstrapPatch
--- PluginManager.cs
+++ PluginManager.cs
@@ -1,7 +1,7 @@
-﻿using BepInEx.Logging;
+﻿using BepInEx.Configuration;
+using BepInEx.IL2CPP.UnityEngine;
+using BepInEx.Logging;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 
 namespace ArrowPlugin;
@@ -27,8 +27,66 @@ public class PluginManager : MonoBehaviour
         return gameObject;
     }
 
+    private void Start()
+    {
+        
+    }
+
     private void Update()
     {
-        Log.LogMessage("Update");
+        //Log.LogMessage("Update");
+        //Log.LogMessage($"Money: {GameDataManager.money}");
+        if (GameManager.Instance)
+        {
+            if (GameManager.Instance.playerInteraction)
+            {
+                //public static ConfigEntry<float> animationSpeed;
+                //public static ConfigEntry<int> projectileCount;
+                //public static ConfigEntry<float> projectileDamage;
+                //public static ConfigEntry<float> projectileDistance;
+                //public static ConfigEntry<float> projectileSpeed;
+                //AddAnimatorSpeed
+                //Log.LogMessage($"GetProjectileSpeed:{PlayerInteraction.bowLevel}");
+                //GameManager.Instance.playerInteraction.AddHealth(1);
+                if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.Keypad1))
+                {
+                    //GameManager.Instance.playerInteraction.health = 1000;
+                    //GameManager.Instance.playerInteraction.animationSpeed = 100;
+                    //GameManager.Instance.playerInteraction.AddAnimatorSpeed(0);
+                    //GameManager.Instance.playerInteraction.projectileCount = 10;
+                    //GameManager.Instance.playerInteraction.projectileDistance = 10;
+                    //GameManager.Instance.playerInteraction.projectileSpeed = 10;
+                    //GameManager.Instance.playerInteraction.AddAnimatorSpeed(0);
+                    //Log.LogMessage($"GetProjectileFrequency: {GameManager.Instance.playerInteraction.GetProjectileFrequency()}");
+                    //Log.LogMessage($"projectileCount:{GameManager.Instance.playerInteraction.projectileCount}");
+                    //Log.LogMessage($"GetProjectileDamage:{GameManager.Instance.playerInteraction.GetProjectileDamage()}");
+                    //Log.LogMessage($"GetProjectileDistance:{GameManager.Instance.playerInteraction.GetProjectileDistance()}");
+                    //Log.LogMessage($"GetProjectileSpeed:{GameManager.Instance.playerInteraction.GetProjectileSpeed()}");
+                    //Log.LogMessage(GameObject.Find("Canvas").transform.GetChild(11).gameObject.name);
+                    //GameObject.Find("Canvas").transform.GetChild(11).gameObject.SetActive(true);
+
+                    //Log.LogMessage(GameObject.Find("Canvas").transform.Find("debugPanel").gameObject.name);
+                    //GameObject.Find("Canvas").transform.Find("debugPanel").gameObject.SetActive(true);
+
+                    //PlayerInteraction.bowLevel += 1;
+
+                    //GameManager.Instance.swordsManager.GenerateMultipleSwords(100);
+                    //Log.LogMessage(FlySwordParentControl.cooldown);
+                    //Log.LogMessage(FlySwordParentControl.moveSpeed);
+                    //Log.LogMessage(FlySwordParentControl.damage);
+
+
+                }
+
+                if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.Keypad2))
+                {
+                    //FlySwordParentControl.cooldown = 0;
+                    //FlySwordParentControl.moveSpeed = 150;
+                    //FlySwordParentControl.damage = 1;
+                }
+            }
+                
+
+        }
     }
 }
